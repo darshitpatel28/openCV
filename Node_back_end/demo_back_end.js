c8eed2ae84798c8c07d3fdf61553f44e6cd5f29a
darshit_28 <79442311+darshitpatel28@users.noreply.github.com>
2024-04-11 09:16:07 +0530

    changes

---
+++ Node_back_end/demo_back_end.js
@@ -0,0 +1,4 @@
+// JS pycharm ma nay chale atle VS ma thi karvu jose... ema thi COMMIT & PUSH... PULL karvu jose JS nu vscode mathi
+// Je code tame commit and push karso ama add thy jase ane next day tya thij pachu avse
+// atle jyare code start karo tyare PULL kari levanu github repo pycharm mathu
+// jyare code finish karo tyare COMMIT & PUSH kari devano
--- Python code/python_demo.py
+++ Python code/python_demo.py
@@ -1,3 +1,3 @@
 # Je code tame commit and push karso ama add thy jase ane next day tya thij pachu avse
 # atle jyare code start karo tyare PULL kari levanu github repo pycharm mathu
-# jyare code finish karo tyare COMMIT & PUSH kari devaano ate github ma store thy jase badha changes
+# jyare code finish karo tyare  COMMIT & PUSH kari devaano ate github ma store thy jase badha changes
