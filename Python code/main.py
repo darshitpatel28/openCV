3601ca2e604d30203e21198b1d3f3efb36e86292
darshitpatel28 <darshitpatel0028@gmail.com>
2024-04-13 22:43:38 +0530

    When object goes far it adjust size corresposnding to it Now started face detect if not id card found

--- Python code/main.py
+++ Python code/main.py
@@ -1,7 +1,7 @@
 import cv2
 import ribbon_detect as detect
 
-user_video = "http//100.115.87.19:8080/video"
-detect.id_card_detect(user_video)
+user_video = 0
+detect.detect(user_video)
 
 cv2.destroyAllWindows()
