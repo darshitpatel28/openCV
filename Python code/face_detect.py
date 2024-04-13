0268e4934da04ab95b7a98d9cc1ce729214e3304
darshit_28 <79442311+darshitpatel28@users.noreply.github.com>
2024-04-13 23:07:12 +0530

    Update face_detect.py

--- Python code/face_detect.py
+++ Python code/face_detect.py
@@ -1,33 +1,22 @@
 import cv2
 
-# Load the cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Function to detect faces and draw rectangles around them
 def detect_faces(image): # a
-    # Convert image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Detect faces
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30))
-    # Draw rectangles around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
     return image
 
-# Capture video from webcam
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Read the frame
     ret, frame = cap.read()
-    # Perform face detection
     frame = detect_faces(frame)
-    # Display the frame
     cv2.imshow('Face Detection', frame)
-    # Break the loop when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the VideoCapture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
